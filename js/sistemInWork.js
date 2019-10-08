afc2982fcb11b1aedba6e66ca2240a001c5f2bd6
nekoshiro046 <nekoshiro046@gmail.com>
2019-10-08 19:02:41 +0900

    updata

--- js/sistemInWork.js
+++ js/sistemInWork.js
@@ -1,24 +1,54 @@
-$(function() {$('html,body').animate({ scrollTop: 0 }, '1');});
 var fr = 60;
 let firstBox;
 var canvas;
+
 let fontSize = 40;
 let font;
 var ww = 5,wh = 5;
-function preload(){font = loadFont('assets/font/FreeSans.otf');}
+function preload(){
+	font = loadFont('assets/font/FreeSans.otf');
+}
+
+
 function setup() {
+  // createCanvas(windowWidth, windowHeight,WEBGL);
   canvas = createCanvas(windowWidth/ww, windowWidth/ww,WEBGL);
+
   canvas.position("30%",0);
   canvas.style('position','fixed');
   canvas.style('z-index','0');
   canvas.parent('sketch-holder');
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
   frameRate(fr);
   firstBox = new box(0,0,0,(windowWidth)/ww/5);
-  textFont(font);textSize(fontSize);textAlign(CENTER, CENTER);textStyle(NORMAL);	rectMode(CENTER);
+	//---------------font------------
+  textFont(font);
+  textSize(fontSize);
+  textAlign(CENTER, CENTER);
+  textStyle(NORMAL);
+	
+  rectMode(CENTER);
 }
-function draw(){drawScene();}
-function windowResized(){resizeCanvas(windowWidth/ww, windowWidth/ww);}
-function mousePressed() {if(firstBox.inTerritory(mouseX,mouseY) && inCanvas()){window.close();}}
+
+
+function draw() {
+	drawScene();
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth/ww, windowWidth/ww);
+}
+
+function mousePressed() {
+	if(firstBox.inTerritory(mouseX,mouseY) && inCanvas()){
+		window.close();
+	}
+}
+
+
+
 function drawScene(){
   background(255);
   if(firstBox.inTerritory(mouseX,mouseY)){
@@ -30,25 +60,49 @@ function drawScene(){
   	}
     stroke(firstBox.baseColor);
     firstBox.drawInnerBox();
+    // firstBox.drawTetrahedron();
 	drawText(firstBox.core.x,firstBox.core.y,'back',firstBox.inch,0);
    }else{
     stroke(0,50);
     drawText(firstBox.core.x,firstBox.core.y,'back',firstBox.inch,150);
    }
    firstBox.drawBox(); 
+  
 }
-function drawText(x,y,str,strSiz,col){push();  translate(x,y);fill(col);textSize(strSiz);text(str, 0,0);pop();}
+
+
+function drawText(x,y,str,strSiz,col){
+	push();  
+	translate(x,y);
+	fill(col);
+	textSize(strSiz);
+	text(str, 0,0);
+	pop();
+}
+
 function inCanvas(){
 	var back;
-	if(mouseX < 0 || mouseX > windowWidth || mouseY < 0 || mouseY > windowHeight){back = false;}else{back = true;}
+	if(mouseX < 0 || mouseX > windowWidth || mouseY < 0 || mouseY > windowHeight){
+		back = false;
+	}else{
+		back = true;
+	}
 	return back;
 }
+
 function objInCanvas(x,y){
 	var back;
-	if(x <  -windowWidth/2|| x > windowWidth/2|| y < -windowHeight/2 || y > windowHeight/2){back = false;}else{back = true;}
+	if(x <  -windowWidth/2|| x > windowWidth/2|| y < -windowHeight/2 || y > windowHeight/2){
+		back = false;
+	}else{
+		back = true;
+	}
 	return back;
 }
+
+//-----------------------------------box_class-----------------------------
 class box{
+
 	constructor(x,y,z,inc,ln){
 		this.core =	createVector(x,y,z);
 		this.inch = inc;
@@ -56,89 +110,98 @@ class box{
 		this.bVertex  = [];
 	    this.velocity = createVector(random(-5,5), random(-5,5));
 	    this.acceleration = createVector(random(-0.05,0.05), random(-0.05,0.05));
+	    // this.firstAc = createVector(random(-5,5),random(-5,5),0);
 		this.head4Speed = random(0.1,0.8);
+
 	    this.linkID = ln;
+
 	    this.angleX = random(360);this.angleY = random(360);this.angleZ = random(360);
 	    this.angleStep = 0.5;
 	    this.baseColor = color(random(255),random(255),random(255));
 	    this.shapeBox();
+
 	    this.innerBVertex  = [];
 	    this.shapeInnerBox();
 	}
 
 	shapeBox(){
-	    var px1 = this.core.x - this.inch;
-	    var px2 = this.core.x + this.inch;
-	    var py1 = this.core.y - this.inch;
-	    var py2 = this.core.y + this.inch;
-	    var pz1 = this.core.z - this.inch;
-	    var pz2 = this.core.z + this.inch;
+	    var pointx1 = this.core.x - this.inch;
+	    var pointx2 = this.core.x + this.inch;
+	    var pointy1 = this.core.y - this.inch;
+	    var pointy2 = this.core.y + this.inch;
+	    var pointz1 = this.core.z - this.inch;
+	    var pointz2 = this.core.z + this.inch;
+
 	    for(let i = 0; i <= 7; i++){
 	      switch(i){
 	        case 0:
-	           this.bVertex[i]  = createVector(px1,py1,pz1);
+	           this.bVertex[i]  = createVector(pointx1,pointy1,pointz1);
 	           break;
 	        case 1:
-	           this.bVertex[i]  = createVector(px1,py1,pz2);
+	           this.bVertex[i]  = createVector(pointx1,pointy1,pointz2);
 	           break;   
 	        case 2:
-	           this.bVertex[i]  = createVector(px1,py2,pz2);
+	           this.bVertex[i]  = createVector(pointx1,pointy2,pointz2);
 	           break;
 	        case 3:
-	           this.bVertex[i]  = createVector(px1,py2,pz1);
+	           this.bVertex[i]  = createVector(pointx1,pointy2,pointz1);
 	           break; 
 	        case 4:
-	           this.bVertex[i]  = createVector(px2,py2,pz1);
+	           this.bVertex[i]  = createVector(pointx2,pointy2,pointz1);
 	           break;
 	        case 5:
-	           this.bVertex[i]  = createVector(px2,py2,pz2);
+	           this.bVertex[i]  = createVector(pointx2,pointy2,pointz2);
 	           break; 
 	        case 6:
-	           this.bVertex[i]  = createVector(px2,py1,pz2);
+	           this.bVertex[i]  = createVector(pointx2,pointy1,pointz2);
 	           break; 
 	        case 7:
-	           this.bVertex[i]  = createVector(px2,py1,pz1);
+	           this.bVertex[i]  = createVector(pointx2,pointy1,pointz1);
 	           break;   
 	      }
 	    }
+    
   	}
 
   	shapeInnerBox(){
-	    var px1 = this.core.x - this.inch/2;
-	    var px2 = this.core.x + this.inch/2;
-	    var py1 = this.core.y - this.inch/2;
-	    var py2 = this.core.y + this.inch/2;
-	    var pz1 = this.core.z - this.inch/2;
-	    var pz2 = this.core.z + this.inch/2;
+	    var pointx1 = this.core.x - this.inch/2;
+	    var pointx2 = this.core.x + this.inch/2;
+	    var pointy1 = this.core.y - this.inch/2;
+	    var pointy2 = this.core.y + this.inch/2;
+	    var pointz1 = this.core.z - this.inch/2;
+	    var pointz2 = this.core.z + this.inch/2;
+
 	    for(let i = 0; i <= 7; i++){
 	      switch(i){
 	        case 0:
-	           this.bVertex[i]  = createVector(px1,py1,pz1);
+	           this.innerBVertex[i]  = createVector(pointx1,pointy1,pointz1);
 	           break;
 	        case 1:
-	           this.bVertex[i]  = createVector(px1,py1,pz2);
+	           this.innerBVertex[i]  = createVector(pointx1,pointy1,pointz2);
 	           break;   
 	        case 2:
-	           this.bVertex[i]  = createVector(px1,py2,pz2);
+	           this.innerBVertex[i]  = createVector(pointx1,pointy2,pointz2);
 	           break;
 	        case 3:
-	           this.bVertex[i]  = createVector(px1,py2,pz1);
+	           this.innerBVertex[i]  = createVector(pointx1,pointy2,pointz1);
 	           break; 
 	        case 4:
-	           this.bVertex[i]  = createVector(px2,py2,pz1);
+	           this.innerBVertex[i]  = createVector(pointx2,pointy2,pointz1);
 	           break;
 	        case 5:
-	           this.bVertex[i]  = createVector(px2,py2,pz2);
+	           this.innerBVertex[i]  = createVector(pointx2,pointy2,pointz2);
 	           break; 
 	        case 6:
-	           this.bVertex[i]  = createVector(px2,py1,pz2);
+	           this.innerBVertex[i]  = createVector(pointx2,pointy1,pointz2);
 	           break; 
 	        case 7:
-	           this.bVertex[i]  = createVector(px2,py1,pz1);
+	           this.innerBVertex[i]  = createVector(pointx2,pointy1,pointz1);
 	           break;   
 	      }
 	    }
+    
   	}
+
 	drawBox(){
 		push();
 		translate(this.core.x,this.core.y);
@@ -146,6 +209,7 @@ class box{
 	    rotateY(radians(this.angleY));this.angleY+= this.angleStep;if(this.angleY > 360)this.angleY=0;
 	    rotateZ(radians(this.angleZ));this.angleZ+= this.angleStep;if(this.angleZ > 360)this.angleZ=0;
 	    noFill();
+
 	    beginShape(); 
 	    for(let i = 0; i< 7; i++){
 	      vertex(this.bVertex[i].x,this.bVertex[i].y,this.bVertex[i].z);
@@ -167,6 +231,7 @@ class box{
 	    }  
 	    pop();
 	}
+
 	drawInnerBox(){
 		push();
 		translate(this.core.x,this.core.y);
@@ -174,6 +239,9 @@ class box{
 	    rotateY(radians(this.angleY));
 	    rotateZ(radians(this.angleZ));
 	    noFill();
+
+	    // point(0,0);
+
 	    beginShape(); 
 	    for(let i = 0; i< 7; i++){
 	      vertex(this.innerBVertex[i].x,this.innerBVertex[i].y,this.innerBVertex[i].z);
@@ -195,15 +263,35 @@ class box{
 	    }  
 	    pop();
 	}
+
 	moveInnerBox(){
 		var pointx1,pointx2,pointy1,pointy2,pointz1,pointz2;
 		var rnX = random(2),rnY = random(2),rnZ = random(2);
-		if(rnX < 1){pointx1 = this.core.x - this.inch/2 + this.inch/2;pointx2 = this.core.x + this.inch/2 + this.inch/2;
-		}else{pointx1 = this.core.x - this.inch/2 - this.inch/2;pointx2 = this.core.x + this.inch/2 - this.inch/2;}
-		if(rnY < 1){pointy1 = this.core.y - this.inch/2 + this.inch/2;pointy2 = this.core.y + this.inch/2 + this.inch/2;
-		}else{pointy1 = this.core.y - this.inch/2 - this.inch/2;pointy2 = this.core.y + this.inch/2 - this.inch/2;}
-		if(rnZ < 1){pointz1 = this.core.z - this.inch/2 + this.inch/2;pointz2 = this.core.z + this.inch/2 + this.inch/2;
-		}else{pointz1 = this.core.z - this.inch/2 - this.inch/2;pointz2 = this.core.z + this.inch/2 - this.inch/2;}
+		if(rnX < 1){
+			pointx1 = this.core.x - this.inch/2 + this.inch/2;
+			pointx2 = this.core.x + this.inch/2 + this.inch/2;
+		}else{
+			pointx1 = this.core.x - this.inch/2 - this.inch/2;
+			pointx2 = this.core.x + this.inch/2 - this.inch/2;
+		}
+
+		if(rnY < 1){
+			pointy1 = this.core.y - this.inch/2 + this.inch/2;
+			pointy2 = this.core.y + this.inch/2 + this.inch/2;
+
+		}else{
+			pointy1 = this.core.y - this.inch/2 - this.inch/2;
+			pointy2 = this.core.y + this.inch/2 - this.inch/2;
+		}
+
+		if(rnZ < 1){
+			pointz1 = this.core.z - this.inch/2 + this.inch/2;
+			pointz2 = this.core.z + this.inch/2 + this.inch/2;
+		}else{
+			pointz1 = this.core.z - this.inch/2 - this.inch/2;
+			pointz2 = this.core.z + this.inch/2 - this.inch/2;
+		}
+
 		for(let i = 0; i <= 7; i++){
 	      switch(i){
 	        case 0:
@@ -232,7 +320,9 @@ class box{
 	           break;   
 	      }
 	    }
+
 	}
+
 	drawTetrahedron(){
 	    push();  
 	    translate(this.core.x,this.core.y);
@@ -240,6 +330,7 @@ class box{
 	    rotateY(radians(this.angleY));
 	    rotateZ(radians(this.angleZ));
 	    noFill();
+	    
 	    beginShape(); 
 	    for(var i = 0; i < 5; i+=2){
 	      vertex(this.bVertex[i].x,this.bVertex[i].y,this.bVertex[i].z);
@@ -256,12 +347,29 @@ class box{
 	    endShape();
 	    pop();
   	}
+
 	inTerritory(mx,my){
 	    var back;
 	    var f = createVector(mx-windowWidth/2/ww,my-windowWidth/2/ww);
 	    var dbox = p5.Vector.sub(f, this.core);
 	    var dist_box = dbox.mag();
-	    if(dist_box < this.territory){back = true;}else{back = false;}
+	    if(dist_box < this.territory){
+	      back = true;
+	    }else{
+	      back = false;
+	    }
 	    return back;
 	}
 }
+var height = window.parent.screen.height;
+
+$(function () {
+    var w = $(window).width();
+    var topBtn = $('#page-top');
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
